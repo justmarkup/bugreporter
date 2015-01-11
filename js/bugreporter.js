45fa525c306ea0e1e66a6fb29ec3388b1e453090
justmarkup <hallo@justmarkup.com>
2015-01-11 15:07:14 +0100

    add detection for device orientation

--- js/bugreporter.js
+++ js/bugreporter.js
@@ -77,6 +77,18 @@
     	return dpr;
 	}
 
+	function getDeviceOrientation() {
+		if (window.orientation && window.DeviceOrientationEvent || window.OrientationEvent || typeof window.onorientationchange != 'undefined') {
+			if (Math.abs(window.orientation) === 90) {
+				return 'landscape';
+			} else {
+				return 'portrait';
+			}
+		}
+		// If we can't get a value assume landscape
+		return 'probably landscape';
+	}
+
 
 	if (hash) {
 		hash = hash.replace(/%20/g, ' ').split(':');
@@ -85,26 +97,28 @@
 		log('<h2>Browserengine</h2> ' + hash[4]);
 		log('<h2>Browser Dimensions</h2> ' + hash[5]);
 		log('<h2>Device Dimensions</h2> ' + hash[6]);
-		log('<h2>Color Depth</h2> ' + hash[7]);
-		if (hash[8] ===  '0') {
+		log('<h2>Device Orientation</h2> ' + hash[7]);
+		log('<h2>Color Depth</h2> ' + hash[8]);
+		if (hash[9] ===  '0') {
 			log('<h2>Flash</h2> not availabe');
 		} else {
-			log('<h2>Flash</h2> Version: ' + hash[8]);
+			log('<h2>Flash</h2> Version: ' + hash[9]);
 		}
-		log('<h2>Cookies</h2> ' + hash[9]);
-		log('<h2>LocalStorage</h2> ' + hash[10]);
-		log('<h2>Adblock Plugin</h2> ' + hash[11]);
-		log('<h2>Browser Language</h2> ' + hash[12]);
-		log('<h2>Pixel Ratio</h2> ' + hash[13]);
-		log('<h2>CSS Resolution</h2> ' + hash[14] + 'dpi');
-		log('<h2>User Agent</h2> <small>' + hash[15] + '</small>');
-		log('<h2>Description</h2> ' + hash[16]);
+		log('<h2>Cookies</h2> ' + hash[10]);
+		log('<h2>LocalStorage</h2> ' + hash[11]);
+		log('<h2>Adblock Plugin</h2> ' + hash[12]);
+		log('<h2>Browser Language</h2> ' + hash[13]);
+		log('<h2>Pixel Ratio</h2> ' + hash[14]);
+		log('<h2>CSS Resolution</h2> ' + hash[15] + 'dpi');
+		log('<h2>User Agent</h2> <small>' + hash[16] + '</small>');
+		log('<h2>Description</h2> ' + hash[17]);
 	} else {
 		log('<h2>OS</h2> ' + platform.os + '');
 		log('<h2>Browser</h2> ' + platform.name + ' (' + platform.version + ')');
 		log('<h2>Browserengine</h2> ' + platform.layout);
 		log('<h2>Browser Dimensions</h2> ' + docW + ' x ' + docH);
 		log('<h2>Device Dimensions</h2> ' + w + ' x ' + h);
+		log('<h2>Device Orientation</h2> ' + getDeviceOrientation());
 		log('<h2>Color Depth</h2> ' + color);
 		if (getFlashVersion() ===  '0') {
 			log('<h2>Flash</h2> not availabe');
@@ -119,7 +133,7 @@
 		log('<h2>CSS Resolution</h2> ' + res.dpi() + 'dpi, ' + res.dppx() + 'dppx, ' + res.dpcm() + 'dpcm');
 		log('<h2>User Agent</h2> <small>' + navigator.userAgent + '</small>');
 		log('<h2>Description</h2> ' + platform.description);
-		doc.location.hash = ':' + platform.os + ':' + platform.name + ':' + platform.version + ':' + platform.layout + ':' + docW + ' x ' + docH + ':' + w + ' x ' + h + ':' + color + ':' + getFlashVersion().split(',').shift() + ':' + cookiesEnabled() + ':' + localStorageEnabeled() + ':' + adblock + ':' + getBrowserLanguage() + ':' + getPixelRatio() + ':' + res.dpi() + 'dpi, ' + res.dppx() + 'dppx, ' + res.dpcm() + 'dpcm' + ':' + navigator.userAgent+ ':' + platform.description;
+		doc.location.hash = ':' + platform.os + ':' + platform.name + ':' + platform.version + ':' + platform.layout + ':' + docW + ' x ' + docH + ':' + w + ' x ' + h + ':' + getDeviceOrientation() + ':' + color + ':' + getFlashVersion().split(',').shift() + ':' + cookiesEnabled() + ':' + localStorageEnabeled() + ':' + adblock + ':' + getBrowserLanguage() + ':' + getPixelRatio() + ':' + res.dpi() + 'dpi, ' + res.dppx() + 'dppx, ' + res.dpcm() + 'dpcm' + ':' + navigator.userAgent+ ':' + platform.description;
 	}
 	var alldetails = document.location.href.replace(/ /g,'').replace('#', '%23');
 	twitter.href = 'https://twitter.com/intent/tweet?text=My browser details:&url=' + alldetails + '&via=justmarkup';
